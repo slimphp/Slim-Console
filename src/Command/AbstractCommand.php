6259b75c5033cdcf3139e1c1a99830267ff2aa1f
Pierre BÃ©rubÃ© <pierre@lgse.com>
2020-05-01 15:05:27 -0600

    create AbstractCommand class

---
+++ src/Command/AbstractCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Slim Framework (https://slimframework.com)
+ *
+ * @license https://github.com/slimphp/Slim-Console/blob/0.x/LICENSE.md (MIT License)
+ */
+
+declare(strict_types=1);
+
+namespace Slim\Console\Command;
+
+use RuntimeException;
+use Slim\Console\Application;
+use Slim\Console\Config\Config;
+use Symfony\Component\Console\Command\Command;
+
+abstract class AbstractCommand extends Command
+{
+    /**
+     * @return Config|null
+     */
+    public function getConfig(): ?Config
+    {
+        $app = $this->getApplication();
+
+        if ($app instanceof Application === false) {
+            throw new RuntimeException('Cannot call method `getConfig()` on ' . get_class($app));
+        }
+
+        return $app->getConfig();
+    }
+}
